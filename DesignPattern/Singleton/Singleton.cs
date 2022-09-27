c157c36c1ccf1239fe5fea958ce88c3769f43e2f
Hector Almonte <hbalmontess272@gmail.com>
2022-09-26 21:38:30 -0300

    Singleton

---
+++ DesignPattern/Singleton/Singleton.cs
@@ -0,0 +1,32 @@
+﻿namespace DesignPattern.Singleton
+{
+    /*
+     * Singleton
+     * 
+     * El patron singleton es un patron de diseño creacional que permite asegurar que una clase tenga solo una
+     * instancia, y proporciona un punto de acceso global a ella.
+     * 
+     * El patron singleton es muy simple de implementar. Solo debemos asegurarnos de que la clase tenga un
+     * constructor privado, un metodo estatico de creacion que actue como un constructor y un campo estatico
+     * que almacene la instancia de la clase.
+     */
+    
+    public class Singleton
+    {
+        private readonly static Singleton _instance = new Singleton();
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        
+        private Singleton()
+        {
+        }
+
+        
+    }
+}
---
+++ DesignPattern_ASP/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using DesignPattern_ASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Tools;
+
+namespace DesignPattern_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            Log.GetInstance("log.txt").Save("Hello World");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            Log.GetInstance("log.txt").Save("Hello World");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
